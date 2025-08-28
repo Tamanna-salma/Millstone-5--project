039461a67b7462f86003ac48fdb2552fd08a7f65
Tamanna-salma <tamannaummey26@gmail.com>
2025-08-28 12:50:44 +0600

    heart increase

--- js/index.js
+++ js/index.js
@@ -1,10 +1,65 @@
-const loveIcon =document.getElementsByClassName('heart');
-// console.log(loveIcon);
-let number =0
+// Increase heart*****
+
+const loveIcon = document.getElementsByClassName('heart');
+let count = 0
 for (const loves of loveIcon) {
-    loves.addEventListener('click',function(){
-        number++
-        const heartbtn =document.getElementById('increase')
-        heartbtn.innerText =number
-    })  
+    loves.addEventListener('click', function () {
+        count++
+        const heartbtn = document.getElementById('increase')
+        heartbtn.innerText = count;
+    })
 }
+
+// call button ***
+
+let points = 100;
+const coin = document.getElementById('point-btn');
+const callbtn = document.getElementsByClassName('call-btn');
+for (let callbutton of callbtn) {
+    callbutton.addEventListener('click',function(){
+        console.log('clicked');
+if(points >=20){
+    points -= 20;
+    coin.innerText =points;
+    alert('20 points')
+
+} else{
+    alert('not point')
+}
+   
+    })
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// callbtn.addEventListener('click', function () {
+//     if (points >= 20) {
+//         points -= 20;
+//         coin.innerText = ;
+//         console.log("coinbtn");
+//         alert('20 point deduct')
+
+//     } else {
+//         alert('not points')
+//     }
+
+// })
+
+
+
+
+
